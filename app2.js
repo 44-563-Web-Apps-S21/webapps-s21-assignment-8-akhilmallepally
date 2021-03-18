66da1d2b7de97268699fdf9978f1087484114376
akhilmallepally <akhilnani831@gmail.com>
2021-03-17 22:57:22 -0500

    added table in app2

--- app2.js
+++ app2.js
@@ -10,10 +10,22 @@ const server =
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
       res.write('<html> <head> <title> Served </title> </head>')
-      res.write('<body>')
+      res.write('<body><style>table, th, td {border: 2px solid black;border-collapse: collapse;}</style>')
       res.write('Content \n')
       res.write('More content \n')
-      res.write('Hello World')    
+      res.write('Hello World')
+      res.write('<table>')
+      res.write('<tr>')
+      res.write('<th>Location name</th>')
+      res.write('<th>Time to be spent</th>')
+      res.write('<th>Approximate expenses</th>')
+      res.write('</tr>')
+      res.write('<tr>')
+      res.write('<td>Kochi</td>')
+      res.write('<td>4 days</td>')
+      res.write('<td>$200</td>')
+      res.write('</tr>')
+      res.write('<tr><td>Mumbai</td><td>4 days</td><td>$300</td></tr><tr><td>Kulu</td><td>5 days</td><td>$300</td></tr><tr><td>Andaman & Nicobar</td><td>7 days</td><td>$500</td></tr></table>')
       res.end('</body></html>')
       //Close the response
     }                           
